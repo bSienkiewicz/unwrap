eada5dd9e593788302e36bb16be8f678629b4f69
Bartek <b.sienkiew@gmail.com>
2024-06-05 12:31:44 +0200

    chore: Add error toast when fetching event data

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Participants from "@/components/Participants";
+import toast from "react-hot-toast";
 
 const fetchData = async (id: string) => {
   try {
@@ -10,9 +11,11 @@ const fetchData = async (id: string) => {
         cache: "no-cache",
       }
     );
+    console.log(response);
     // TODO: Error with passing the data to server. Fix it.
     return response.json();
   } catch (error) {
+    toast.error("Error fetching event data");
     console.error(error);
     return null;
   }
@@ -23,22 +26,32 @@ const SharePage = async ({ params }: { params: { id: string } }) => {
   const participants = data.eventParticipants.rows;
   const details = data.eventDetails.rows[0];
   return (
-    <div className="w-full max-w-[1200px] mx-auto">
-      <div className="my-11">
-        <h1 className="text-3xl font-bold text-center">
-          Share the links with your friends!
-        </h1>
-        <h1 className="text-2xl mt-12 font-bold">{details.name}</h1>
-        {details.description && (
-          <p className="italic text-neutral-600">{details.description}</p>
-        )}
-      </div>
+    <>
+      {data ? (
+        <div className="w-full max-w-[1200px] mx-auto">
+          <div className="my-11">
+            <h1 className="text-3xl font-bold text-center">
+              Share the links with your friends!
+            </h1>
+            <h1 className="text-2xl mt-12 font-bold">{details.name}</h1>
+            {details.description && (
+              <p className="italic text-neutral-600">{details.description}</p>
+            )}
+          </div>
 
-      <p className="mb-3">Click your friend's name to copy his unique link:</p>
-      <div className="flex flex-col gap-3">
-          <Participants participants={participants} />
-      </div>
-    </div>
+          <p className="mb-3">
+            Click your friend's name to copy his unique link:
+          </p>
+          <div className="flex flex-col gap-3">
+            <Participants participants={participants} />
+          </div>
+        </div>
+      ) : (
+        <div className="w-full max-w-[1200px] mx-auto">
+          <h1 className="text-3xl font-bold text-center">Event not found</h1>
+        </div>
+      )}
+    </>
   );
 };
 
